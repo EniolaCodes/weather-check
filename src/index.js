33c88c9c6a2b4713bf0607a0616f0e4965e6d2fa
EniolaCodes <adebimpeniola@gmail.com>
2023-04-19 20:05:01 +0100

    Restructured html and styled in css

--- src/index.js
+++ src/index.js
@@ -52,6 +52,12 @@ function weatherCondition(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
 }
 
 function searchLocation(position) {
