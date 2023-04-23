a2d3813b7a51b8ce29efec1b8019fb70986d25c2
EniolaCodes <adebimpeniola@gmail.com>
2023-04-23 12:32:38 +0100

    Updated js file

--- src/index.js
+++ src/index.js
@@ -24,7 +24,8 @@ let days = [
 let day = days[now.getDay()];
 document.querySelector("#time").innerHTML = `${day} ${h}:${m}`;
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = ` <div class="row">`;
@@ -65,6 +66,14 @@ function search(city) {
   axios.get(apiUrl).then(weatherCondition);
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "8829e8c6941f34c584905acfa8203214";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function weatherCondition(response) {
   celsiusTemperature = response.data.main.temp;
   document.querySelector("#city").innerHTML = response.data.name;
@@ -86,11 +95,12 @@ function weatherCondition(response) {
       "src",
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
+  getForecast(response.data.coord);
 }
 
 function searchLocation(position) {
   console.log(position);
-  let apiKey = "3b7f41f709e5fa9dea292c8a3c7b4b11";
+  let apiKey = "8829e8c6941f34c584905acfa8203214";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
   axios.get(apiUrl).then(weatherCondition);
